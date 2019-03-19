a433cf5bf3cb628f3fcae1e2dacb7ea5a585d6d8
Julia Magyar <magyar.juliar@gmail.com>
2019-03-19 18:01:50 -0400

    Updates

---
+++ precache-manifest.7e9113a22d42ffb1552224fd07a18f94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "e528207c042d49311caa",
+    "url": "/clickygame/static/js/main.c0132e4b.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/clickygame/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "e528207c042d49311caa",
+    "url": "/clickygame/static/css/main.16ee42f8.chunk.css"
+  },
+  {
+    "revision": "ad2667ab3b793a53d0ebc1ff1c683e1a",
+    "url": "/clickygame/index.html"
+  }
+];
